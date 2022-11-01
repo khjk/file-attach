ad2ec619fb851a8ea3aa840358a5fcaf48a064bd
khjk <begywjd@naver.com>
2022-11-01 17:13:06 +0900

    Initial commit

---
+++ src/main/java/flow/fileattach/FileAttachApplication.java
@@ -0,0 +1,13 @@
+package flow.fileattach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAttachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileAttachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flow/fileattach/FileAttachApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.fileattach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAttachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
